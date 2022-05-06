9918dd744e41a2680c0420d190c898981e9d30cf
Ismail Kara <ismail.kara@bilgeadamboost.com>
2022-05-07 02:36:02 +0300

    review

--- js/quiz.js
+++ js/quiz.js
@@ -1,39 +1,37 @@
 const quizData = [
-    {
-        question: "Türkiye'nin başkenti aşağıdakilerden hangisidir ?",
-        a: "Ankara",
-        b: "İstanbul",
-        c: "Sivas",
-        d: "İzmir",
-        correct: "a"
-    },
-    {
-        question: "Aşağıdakilerden hangisi HTML kısatlmasının açılımıdır ?",
-        a: "Hightext Market Language",
-        b: "Highlevel Making Language",
-        c: "Hypertext Markup Language",
-        d: "Highline Market Language",
-        correct: "c",
-    },
-    {
-        question: "Aşağıdakilerden hangisi CSS kısaltmasının açılımıdır ?",
-        a: "Cascade Surrounding Style",
-        b: "Casting Style Surface",
-        c: "Cascading Style Sheets",
-        d: "Canvas Sheets Style",
-        correct: "c",
-    }
-
+  {
+    question: "Türkiye'nin başkenti aşağıdakilerden hangisidir ?",
+    a: "Ankara",
+    b: "İstanbul",
+    c: "Sivas",
+    d: "İzmir",
+    correct: "a",
+  },
+  {
+    question: "Aşağıdakilerden hangisi HTML kısatlmasının açılımıdır ?",
+    a: "Hightext Market Language",
+    b: "Highlevel Making Language",
+    c: "Hypertext Markup Language",
+    d: "Highline Market Language",
+    correct: "c",
+  },
+  {
+    question: "Aşağıdakilerden hangisi CSS kısaltmasının açılımıdır ?",
+    a: "Cascade Surrounding Style",
+    b: "Casting Style Surface",
+    c: "Cascading Style Sheets",
+    d: "Canvas Sheets Style",
+    correct: "c",
+  },
 ];
 
-const quiz = document.getElementById('quiz');
-const answerEls = document.querySelectorAll('.answer');
-const questionEl = document.getElementById('question');
-const aText = document.getElementById('a_text');
-const bText = document.getElementById('b_text');
-const cText = document.getElementById('c_text');
-const dText = document.getElementById('d_text');
-const submitBtn = document.getElementById('submit');
+const answerEls = document.querySelectorAll(".answer");
+const questionEl = document.getElementById("question");
+const aText = document.getElementById("a_text");
+const bText = document.getElementById("b_text");
+const cText = document.getElementById("c_text");
+const dText = document.getElementById("d_text");
+const submitBtn = document.getElementById("submit");
 
 let currentQuiz = 0;
 let score = 0;
@@ -41,51 +39,50 @@ let score = 0;
 loadQuiz();
 
 function loadQuiz() {
-    deselectAnswer()
+  deselectAnswer();
 
-    const currentQuizData = quizData[currentQuiz];
+  const currentQuizData = quizData[currentQuiz];
 
-    questionEl.innerText = currentQuizData.question;
-    aText.innerText = currentQuizData.a;
-    bText.innerText = currentQuizData.b;
-    cText.innerText = currentQuizData.c;
-    dText.innerText = currentQuizData.d;
+  questionEl.innerText = currentQuizData.question;
+  aText.innerText = currentQuizData.a;
+  bText.innerText = currentQuizData.b;
+  cText.innerText = currentQuizData.c;
+  dText.innerText = currentQuizData.d;
 }
 
 function deselectAnswer() {
-    answerEls.forEach(answerEl => answerEl.checked = false);
+  answerEls.forEach((answerEl) => (answerEl.checked = false));
 }
 
 function getSelected() {
-    let answer;
-    answerEls.forEach(answerEl => {
-        if (answerEl.checked) {
-            answer = answerEl.id
-        }
-    })
-    return answer
-}
-submitBtn.addEventListener('click', () => {
-    const answer = getSelected()
-    const applouse = "Tebrikler!";
-    if (answer) {
-        if (answer === quizData[currentQuiz].correct) {
-            score++;
-        }
-        currentQuiz++
-        if (currentQuiz < quizData.length) {
-            loadQuiz();
-        } else {
-            if (score == quizData.length) {
-                quiz.innerHTML = `
-                <h2>${applouse} ${score} soruyu doğru cevapladınız.</h2>
-                <button onclick ="location.reload()">Yeniden Yükle</button>`
-            } else {
-                quiz.innerHTML = `
-                <h2>${score} soruyu doğru cevapladınız.</h2>
-                <button onclick ="location.reload()">Tekrar Dene</button>`
-            }
-        }
-
+  let answer;
+  answerEls.forEach((answerEl) => {
+    if (answerEl.checked) {
+      answer = answerEl.id;
     }
-})
+  });
+  return answer;
+}
+submitBtn.addEventListener("click", () => {
+  const answer = getSelected();
+  const applouse = "Tebrikler!";
+  if (answer) {
+    if (answer === quizData[currentQuiz].correct) {
+      score++;
+    }
+    currentQuiz++;
+    if (currentQuiz < quizData.length) {
+      loadQuiz();
+    } else {
+      if (score == quizData.length) {
+        quiz.innerHTML = `
+                <h2>${applouse} ${score} soruyu doğru cevapladınız.</h2>
+                <button onclick ="location.reload()">Yeniden Yükle</button>`;
+      } else {
+        quiz.innerHTML = `
+                <h2>${score} soruyu doğru cevapladınız.</h2>
+                <button onclick ="location.reload()">Tekrar Dene</button>`;
+      }
+    }
+  }
+});
